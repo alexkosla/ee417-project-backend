465dc2883e913e08b0568f89eaaa45670e5ba7de
alexkosla <alexkosla@hotmail.com>
2023-04-06 20:21:33 +0100

    initial commit

---
+++ src/test/java/com/ee417/groupf/GroupFApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ee417.groupf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupFApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
